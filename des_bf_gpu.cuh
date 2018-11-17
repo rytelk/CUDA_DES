8befd8e8957dcaaad4e56f33c68cd09419ee9034
rytelk <krystian.rytel@gmail.com>
2018-11-17 16:01:07 +0100

    test

--- des_bf_gpu.cuh
+++ des_bf_gpu.cuh
@@ -10,14 +10,14 @@
 #include "des_constant_cpu.cuh"
 #include "des_utils.cuh"
 
-__device__ void gpu_brute_force(char *key_alphabet, uint64_t key_alphabet_length, uint32_t key_length, char *message_alphabet, uint64_t message_alphabet_length,
-    uint32_t message_length, uint64_t ciphertext, uint64_t *message_result, uint64_t *key_result, bool *found_key);
+__device__ void gpu_brute_force(char *key_alphabet, int64_t key_alphabet_length, int key_length, char *message_alphabet, int64_t message_alphabet_length,
+    int message_length, uint64_t ciphertext, uint64_t *message_result, uint64_t *key_result, bool *found_key);
     
 __host__ void des_brute_force_gpu(char *key_alphabet, int key_length, char *message_alphabet, int message_length, uint64_t ciphertext);
 
 
-__device__ void gpu_brute_force(char *key_alphabet, uint64_t key_alphabet_length, uint32_t key_length, char *message_alphabet, uint64_t message_alphabet_length,
-    uint32_t message_length, uint64_t ciphertext, uint64_t *message_result, uint64_t *key_result, bool *found_key)
+__device__ void gpu_brute_force(char *key_alphabet, int64_t key_alphabet_length, int key_length, char *message_alphabet, int64_t message_alphabet_length,
+                     int message_length, uint64_t ciphertext)
 {
     uint64_t keys_count = get_combinations_count(key_alphabet_length, key_length);
     uint64_t messages_cout = get_combinations_count(message_alphabet_length, message_length);
@@ -35,9 +35,6 @@ __device__ void gpu_brute_force(char *key_alphabet, uint64_t key_alphabet_length
             //print_hex(message, "Message_" + std::to_string(j));
             if (ciphertext == des_encrypt(message, subkeyes, cpu_IP, cpu_IP_REV, cpu_E_BIT, cpu_P, cpu_S))
             {
-                *key_result = key;
-                *message_result = message;
-                *found_key = true;
                 return;
             }
         }
@@ -51,11 +48,10 @@ __host__ void des_brute_force_gpu(char *key_alphabet, int key_length, char *mess
     uint64_t *message;
     bool *found_key;
 
-    uint64_t key_alphabet_length = (uint64_t)std::strlen(key_alphabet);
-    uint64_t message_alphabet_length = (uint64_t)std::strlen(message_alphabet);
+    int64_t key_alphabet_length = (int64_t)std::strlen(key_alphabet);
+    int64_t message_alphabet_length = (int64_t)std::strlen(message_alphabet);
 
     cudaMallocManaged(&key_alphabet, key_alphabet_length*sizeof(char));
-    cudaMallocManaged(&message_alphabet, message_alphabet_length*sizeof(char));
     cudaMallocManaged(&message_alphabet, message_alphabet_length*sizeof(char));
     cudaMallocManaged(&key, sizeof(uint64_t));
     cudaMallocManaged(&message, sizeof(uint64_t));
@@ -64,7 +60,7 @@ __host__ void des_brute_force_gpu(char *key_alphabet, int key_length, char *mess
     std::chrono::steady_clock::time_point cpu_start, cpu_end;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     gpu_brute_force<<1, 1>>(key_alphabet, key_alphabet_length, key_length, message_alphabet, message_alphabet_length,
-        message_length, ciphertext, message, key, found_key);
+        message_length, ciphertext);
 
     cudaDeviceSynchronize();
     cudaFree(key_alphabet);
