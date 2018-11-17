c73b711cea825e343d8451c3bffb34e9fce056f9
rytelk <krystian.rytel@gmail.com>
2018-11-17 17:47:21 +0100

    f

--- des_bf_gpu.cuh
+++ des_bf_gpu.cuh
@@ -24,7 +24,8 @@ __global__ void gpu_brute_force(char *key_alphabet, int64_t key_alphabet_length,
     uint64_t keys_count = get_combinations_count(key_alphabet_length, key_length);
     uint64_t messages_cout = get_combinations_count(message_alphabet_length, message_length);
     uint64_t subkeyes[16];
-    prtinf("\n--- GPU PARAMS --- ");
+
+    printf("\n--- GPU PARAMS --- ");
     printf("message_alphabet %s\n", key_alphabet);
     printf("key_alphabet_length %d\n", key_alphabet_length);
     printf("key_length %d\n", key_length);
